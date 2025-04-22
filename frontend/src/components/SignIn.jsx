dcbe96ea0a8519f1dde8bf6ce5ed2d12fb06f324
Kayden Lancaster <krazin0202@gmail.com>
2025-04-22 13:26:23 -0600

    hopefully I configed my signing page to use my backend correctly. I also added django CORS stuff.

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -7,7 +7,7 @@ function SignIn() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch('/api/login/', {
+        const response = await fetch('http://127.0.0.1:8000/api/login/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
